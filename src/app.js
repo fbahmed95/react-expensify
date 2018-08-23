11db18cad14dc55254f12e7d6ff3da1450f9b3c2
Fareeha Ahmed <fbahmed@ucdavis.edu>
2018-08-23 11:01:02 -0700

    removed dummy data from app.js

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'rent', amount: 109500}));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 3000)
+// store.dispatch(addExpense({description: 'water bill', amount: 4500}));
+// store.dispatch(addExpense({description: 'gas bill', createdAt: 1000}));
+// store.dispatch(addExpense({description: 'rent', amount: 109500}));
+// store.dispatch(setTextFilter('water'));
+//
+// setTimeout(() => {
+//   store.dispatch(setTextFilter('bill'));
+// }, 3000)
 
 
 const state = store.getState();
